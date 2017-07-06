a3ca311f5e3b39076405d26d5f0bb09fd7da9c20
Christian Alfoni <christianalfoni@gmail.com>
2017-07-06 12:37:41 +0200

    Fixed no previous release handling

--- src/Cooker/providers/GitProvider/getHashListFromHash.js
+++ src/Cooker/providers/GitProvider/getHashListFromHash.js
@@ -3,6 +3,7 @@ const Git = require('nodegit')
 function getHashListFromHashToCommit(sha, commit) {
   return new Promise((resolve, reject) => {
     let done = false
+    let timeout = null
     const list = []
     commit
       .history()
@@ -11,9 +12,12 @@ function getHashListFromHashToCommit(sha, commit) {
           // discard
         } else if (commit.sha() === sha) {
           done = true
+          clearTimeout(timeout)
           resolve(list)
         } else {
+          clearTimeout(timeout)
           list.unshift(commit.sha())
+          timeout = setTimeout(() => resolve(list))
         }
       })
       .start()
