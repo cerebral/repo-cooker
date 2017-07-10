d892e2d038e496ce3e737038369bd52c32adeebc
Gaspard Bucher <gaspard@lucidogen.io>
2017-07-10 21:21:27 +0200

    test(repo-cooker): add test for logging, add coverage script

---
+++ src/Cooker/execCommand.test.js
@@ -0,0 +1,23 @@
+/* eslint-env mocha */
+import assert from 'test-utils/assert'
+import simple from 'simple-mock'
+import { logCommand } from './execCommand'
+
+describe('execCommand', () => {
+  const logs = []
+  before(() => {
+    simple.mock(console, 'log').callFn((...args) => logs.push(args))
+  })
+
+  after(() => simple.restore())
+
+  it('should output commands to console', done => {
+    logCommand('hello', ['a', 'b', 'c'], { some: 'option' }).then(() => {
+      assert.deepEqual(
+        [ ['\nhello\n    a\n    b\n    c\n    {"some":"option"}\n'] ],
+        logs,
+        done
+      )
+    })
+  })
+})
