f9fc10b93915c42b1dd631ea6e90aab952c1d919
Gaspard Bucher <gaspard@lucidogen.io>
2017-09-09 11:03:07 +0200

    style(prettier): fix white space for prettier

--- src/actions/publishUnderTemporaryNpmTag.js
+++ src/actions/publishUnderTemporaryNpmTag.js
@@ -3,18 +3,16 @@ export function publishUnderTemporaryNpmTag({ npm, props }) {
 
   // Need to ensure successful release of all packages, so
   // we publish under a temporary tag first
-  return Promise.all(
-    packages.map(name => npm.publish(name, 'releasing'))
-  ).then(temporaryNpmTagByPackage => ({
-    temporaryNpmTagByPackage: packages.reduce(
-      (temporaryNpmTagByPackage, name) => {
-        temporaryNpmTagByPackage[name] = 'releasing'
+  return Promise.all(packages.map(name => npm.publish(name, 'releasing')))
+    .then(temporaryNpmTagByPackage => ({
+      temporaryNpmTagByPackage: packages.reduce(
+        (temporaryNpmTagByPackage, name) => {
+          temporaryNpmTagByPackage[name] = 'releasing'
 
-        return temporaryNpmTagByPackage
-      },
-      {}
-    ),
-  })).then(
-    arg => new Promise(resolve => setTimeout(() => resolve(arg), 3000))
-  )
+          return temporaryNpmTagByPackage
+        },
+        {}
+      ),
+    }))
+    .then(arg => new Promise(resolve => setTimeout(() => resolve(arg), 3000)))
 }
