65eebf5b30f34e6a20c66399cd7f42d9ab3073dd
Gaspard Bucher <gaspard@lucidogen.io>
2017-09-09 14:08:12 +0200

    fix(repo-cooker): revert previous change and continue using peerDeps
    This reverts commit 0e6c303d20032e2b53c5c57350099cc9433df192.
    It seems this was a bad idea and we should fix circular deps another way.

--- src/Cooker/providers/PackageJsonProvider/getRelatedPackages.test.js
+++ src/Cooker/providers/PackageJsonProvider/getRelatedPackages.test.js
@@ -7,46 +7,22 @@ it('should get related packages by package', function(done) {
   const getRelatedPackages = getRelatedPackagesFactory(config)
 
   getRelatedPackages('@repo-cooker-test/commis').then(relatedPackages => {
-    assert.deepEqual(
-      relatedPackages,
-      {
-        dependencies: ['@repo-cooker-test/poissonier'],
-        devDependencies: [],
-        peerDependencies: [],
-      },
-      done
-    )
+    assert.deepEqual(relatedPackages, ['@repo-cooker-test/poissonier'], done)
   })
 })
 
-it('should read devDependencies', function(done) {
+it('should get read devDependencies', function(done) {
   const getRelatedPackages = getRelatedPackagesFactory(config)
 
   getRelatedPackages('@repo-cooker-test/poissonier').then(relatedPackages => {
-    assert.deepEqual(
-      relatedPackages,
-      {
-        dependencies: [],
-        devDependencies: ['@repo-cooker-test/entremetier'],
-        peerDependencies: [],
-      },
-      done
-    )
+    assert.deepEqual(relatedPackages, ['@repo-cooker-test/entremetier'], done)
   })
 })
 
-it('should read peerDependencies', function(done) {
+it('should get read peerDependencies', function(done) {
   const getRelatedPackages = getRelatedPackagesFactory(config)
 
   getRelatedPackages('@repo-cooker-test/pastry-chef').then(relatedPackages => {
-    assert.deepEqual(
-      relatedPackages,
-      {
-        dependencies: [],
-        devDependencies: [],
-        peerDependencies: ['@repo-cooker-test/sous-chef'],
-      },
-      done
-    )
+    assert.deepEqual(relatedPackages, ['@repo-cooker-test/sous-chef'], done)
   })
 })
