469097263021136799dccd4ca3287cfa4fffeafc
Gaspard Bucher <gaspard@lucidogen.io>
2018-06-03 23:56:34 +0200

    feat(binary): add a repo-cooker binary

---
+++ src/cli.js
@@ -0,0 +1,60 @@
+#!/usr/bin/env node
+// repo-cooker [cmd] [props] --dry-run --devtools
+// 1. Looks for [cmd] in
+//    './[repo-cooker path]/cmd.js'
+//    './[repo-cooker path]/cmd/index.js'
+//    The `[repo-cooker path]` can be defined in package.json
+//    with 'repo-cooker-path' or is set to the default './repo-cooker'
+// 2. If [cmd] is not found, uses the builtin
+//    `run` command to run the given [cmd] in all packages
+//    that contain the given script.
+import { readFileSync, statSync } from 'fs'
+import { join, resolve } from 'path'
+
+import { parseArgs } from './helpers/parseArgs'
+
+const rootPath = '.'
+
+function fileExist(path) {
+  try {
+    const stats = statSync(path)
+    return stats.isFile()
+  } catch (err) {
+    return false
+  }
+  return false
+}
+
+const packagePath = join(rootPath, 'package.json')
+const packageInfo = JSON.parse(readFileSync(packagePath, { encoding: 'utf8' }))
+const receiptsPath = packageInfo['repo-cooker-path'] || 'repo-cooker'
+
+const { cmd, args } = parseArgs(process.argv)
+
+if (!cmd) {
+  throw new Error(`Invalid call to repo-cooker binary: missing command to run.`)
+}
+
+const cmdPath = [
+  join(receiptsPath, `${cmd}.js`),
+  join(receiptsPath, `${cmd}/index.js`),
+].find(fileExist)
+
+let fullCmdPath = undefined
+if (cmdPath) {
+  // Found a custom script for this command.
+  fullCmdPath = resolve(cmdPath)
+  console.log(`Running script: ${fullCmdPath}`)
+} else {
+  fullCmdPath = resolve(join(receiptsPath, 'index.js'))
+  if (!fileExist(fullCmdPath)) {
+    throw new Error(`Missing repo-cooker base file at '${fullCmdPath}'.`)
+  }
+  args.unshift(cmd)
+  args.unshift('--run')
+  // Run the npm script
+  console.log(`No custom script found for '${cmd}' in '${receiptsPath}'`)
+  console.log(`Running npm script '${cmd}'.`)
+}
+// exec babel-node fullCmdPath args
+console.log(`Exec: babel-node ${fullCmdPath} ${args.join(' ')}`)
---
+++ src/helpers/parseArgs.js
@@ -0,0 +1,33 @@
+const optionRe = /^--.+$/
+const validOption = {
+  '--dry-run': true,
+  '--devtools': true,
+}
+
+export function parseArgs(allArgs) {
+  let cmd = undefined
+  const cmdArgs = []
+  const args = allArgs.slice()
+  const nodeExecutable = args.shift()
+  const script = args.shift()
+
+  for (let arg of args) {
+    const isOpt = optionRe.exec(arg)
+    if (isOpt) {
+      // option
+      if (!validOption[arg]) {
+        throw new Error(
+          `Invalid option '${arg}'. Known options are (${Object.keys(
+            validOption
+          ).join(', ')}).`
+        )
+      }
+      cmdArgs.push(arg)
+    } else if (!cmd) {
+      cmd = arg
+    } else {
+      cmdArgs.push(arg)
+    }
+  }
+  return { cmd, args: cmdArgs }
+}
