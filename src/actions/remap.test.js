eec0c853f5361249356c2385aa80e7ceefe39bb0
Gaspard Bucher <gaspard@lucidogen.io>
2018-06-15 15:38:37 +0200

    fix(remap): remap should accept async results like cook.filter

--- src/actions/remap.test.js
+++ src/actions/remap.test.js
@@ -25,4 +25,26 @@ describe('remap', () => {
       done
     )
   })
+
+  it('should remap given prop with async', done => {
+    const someObject = {
+      foo: 'is foo',
+      bar: 'is bar',
+      baz: 'is baz',
+    }
+    const hash = 'ho!'
+    const remapedObject = {
+      foo: 'foo is foo (ho!)',
+      bar: 'bar is bar (ho!)',
+      baz: 'baz is baz (ho!)',
+    }
+    testAction(
+      remap('someObject', (key, value, { props }) =>
+        Promise.resolve(`${key} ${value} (${props.hash})`)
+      ),
+      { hash, someObject },
+      { someObject: remapedObject },
+      done
+    )
+  })
 })
