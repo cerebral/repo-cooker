4e6d5166a57a4699d4531b585e2cc3fde0564b18
Gaspard Bucher <gaspard@lucidogen.io>
2018-06-15 15:38:37 +0200

    fix(remap): remap should accept async results like cook.filter

--- src/actions/remap.js
+++ src/actions/remap.js
@@ -1,13 +1,17 @@
 export function remap(propKey, callback) {
   return function remap(ctx) {
     const base = ctx.props[propKey]
-    return {
-      [propKey]: Object.assign(
-        {},
-        ...Object.keys(base).map(key => ({
-          [key]: callback(key, base[key], ctx),
-        }))
-      ),
-    }
+    return Promise.all(
+      Object.keys(base).map(key => {
+        const value = callback(key, base[key], ctx)
+        if (value instanceof Promise) {
+          return value.then(v => ({ [key]: v }))
+        } else {
+          return Promise.resolve({ [key]: value })
+        }
+      })
+    )
+      .then(results => Object.assign({}, ...results))
+      .then(newProp => ({ [propKey]: newProp }))
   }
 }
