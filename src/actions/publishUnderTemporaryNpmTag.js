32a95e9b61fd9a8f1dfa231437956c03f9e1a01a
Gaspard Bucher <gaspard@lucidogen.io>
2018-06-26 12:05:31 +0200

    fix(release): should not try to release private package

--- src/actions/publishUnderTemporaryNpmTag.js
+++ src/actions/publishUnderTemporaryNpmTag.js
@@ -1,4 +1,9 @@
-export function publishUnderTemporaryNpmTag({ config, npm, props }) {
+export function publishUnderTemporaryNpmTag({
+  config,
+  npm,
+  packageJson,
+  props,
+}) {
   const packages = Object.keys(props.newVersionByPackage).filter(
     name =>
       props.newVersionByPackage[name] !== props.currentVersionByPackage[name]
@@ -6,21 +11,29 @@ export function publishUnderTemporaryNpmTag({ config, npm, props }) {
 
   // Need to ensure successful release of all packages, so
   // we publish under a temporary tag first
-  return Promise.all(packages.map(name => npm.publish(name, 'releasing')))
-    .then(temporaryNpmTagByPackage => ({
-      temporaryNpmTagByPackage: packages.reduce(
-        (temporaryNpmTagByPackage, name) => {
-          temporaryNpmTagByPackage[name] = 'releasing'
+  return Promise.all(
+    packages.map(name =>
+      packageJson.get(name).then(info => (info.private ? null : name))
+    )
+  )
+    .then(names => names.filter(name => name !== null))
+    .then(names =>
+      Promise.all(names.map(name => npm.publish(name, 'releasing')))
+        .then(temporaryNpmTagByPackage => ({
+          temporaryNpmTagByPackage: packages.reduce(
+            (temporaryNpmTagByPackage, name) => {
+              temporaryNpmTagByPackage[name] = 'releasing'
 
-          return temporaryNpmTagByPackage
-        },
-        {}
-      ),
-    }))
-    .then(
-      arg =>
-        config.dryRun
-          ? arg
-          : new Promise(resolve => setTimeout(() => resolve(arg), 3000))
+              return temporaryNpmTagByPackage
+            },
+            {}
+          ),
+        }))
+        .then(
+          arg =>
+            config.dryRun
+              ? arg
+              : new Promise(resolve => setTimeout(() => resolve(arg), 3000))
+        )
     )
 }
