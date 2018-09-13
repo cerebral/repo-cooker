2f8e2fcefed8c1995502edd3d0f6d42d00553a0f
christianalfoni <christianalfoni@gmail.com>
2018-09-13 20:04:08 +0200

    fix(releaseNotes): do not show private packages in release notes

--- src/actions/releaseNotes/avatar.js
+++ src/actions/releaseNotes/avatar.js
@@ -4,8 +4,9 @@ function createNewVersionsTable(release) {
   const entries = Object.keys(release.newVersionByPackage)
     .filter(packageName => {
       return (
+        release.currentVersionByPackage[packageName] &&
         release.currentVersionByPackage[packageName] !==
-        release.newVersionByPackage[packageName]
+          release.newVersionByPackage[packageName]
       )
     })
     .map(packageName => {
