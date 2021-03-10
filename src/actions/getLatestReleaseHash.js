b9a85d7276d8322e50bca466f7575807f2f66582
Maia Taewana <maia@pawijs.org>
2021-03-11 11:53:19 +1300

    fix: latest release hash detection should use both release_[date] and v[date] formats

--- src/actions/getLatestReleaseHash.js
+++ src/actions/getLatestReleaseHash.js
@@ -1,6 +1,8 @@
+export const RELEASE_RE = /[a-zA-Z_]+\d\d\d\d-\d\d-\d\d_\d\d\d\d/
+
 export function getLatestReleaseHash({ git }) {
   return git
-    .getLatestTagMatchingName('release_')
+    .getLatestTagMatchingName(RELEASE_RE)
     .then(tag =>
       tag ? { hash: tag.hash, tag: tag.tag } : { hash: 'Big Bang' }
     )
--- src/actions/tagCurrentCommit.js
+++ src/actions/tagCurrentCommit.js
@@ -1,10 +1,18 @@
+import { RELEASE_RE } from './getLatestReleaseHash'
+
 export function tagCurrentCommit({ git }) {
   const date = new Date().toISOString()
   const releaseDate = date
     .slice(0, 16)
     .replace('T', '_')
     .replace(':', '')
-  // We have to prefix with "v" due to GitHub constraint
+  // This is used to detect latest release in getLatestReleaseHash
+  // THIS NAME MUST MATCH RELEASE_RE
   const name = `v${releaseDate}`
+  if (!RELEASE_RE.test(name)) {
+    // This should NEVER EVER EVER happen :-)
+    throw new Error(`Tag '${name}' does not match ${RELEASE_RE} !!`)
+  }
+
   return git.createTagForCommit(name).then(() => ({ tag: { name, date } }))
 }
