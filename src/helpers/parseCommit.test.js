1ab781e0ea8af3996d4ec15634f3f325fe3fa90f
Henri Hulski <henri.hulski@gazeta.pl>
2023-06-04 18:09:24 +0200

    style: fix linting errors with prettier

--- src/helpers/parseCommit.test.js
+++ src/helpers/parseCommit.test.js
@@ -4,9 +4,6 @@ import { commits, parsedCommits } from 'test-utils/commits'
 import assert from 'test-utils/assert'
 import { parseCommit } from './parseCommit'
 
-it(
-  'should parse commits for breaking changes, issues, scope, etc',
-  done => {
-    assert.deepEqual(commits.map(parseCommit), parsedCommits, done)
-  }
-)
+it('should parse commits for breaking changes, issues, scope, etc', done => {
+  assert.deepEqual(commits.map(parseCommit), parsedCommits, done)
+})
